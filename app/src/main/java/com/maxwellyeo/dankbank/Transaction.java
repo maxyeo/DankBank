27e0b632a8d18a5846893480e9ca1c5c4abe23ec
Max Yeo <mzwyeo@gmail.com>
2016-02-17 00:53:43 -0500

    transaction layout buttons looking swell, starting activity life cycle for transaction

--- app/src/main/java/com/maxwellyeo/dankbank/Transaction.java
+++ app/src/main/java/com/maxwellyeo/dankbank/Transaction.java
@@ -1,26 +1,39 @@
 package com.maxwellyeo.dankbank;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.ActionBar;
+import android.preference.PreferenceManager;
 import android.view.MenuItem;
-
-/**
- * Created by wyao on 16-02-16.
- */
-
+import android.widget.TextView;
 
 public class Transaction extends Activity {
+
+    private Float balance;
+    private TextView msg;
+    private SharedPreferences myPrefs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.transaction);
 
-
+        Context context = getApplicationContext();  // app level storage
+        myPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        balance = myPrefs.getFloat("balance", 0);
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
 
+        SharedPreferences.Editor peditor = myPrefs.edit();
+        peditor.putFloat("balance", balance);
+        peditor.commit();
+    }
 
 }
 
