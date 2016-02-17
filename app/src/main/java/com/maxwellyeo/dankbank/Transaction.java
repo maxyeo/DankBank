222a1e395da5d4fc63565a7c4c2c0b487597ade1
Max Yeo <mzwyeo@gmail.com>
2016-02-17 15:56:37 -0500

    implemented undo button

--- app/src/main/java/com/maxwellyeo/dankbank/Transaction.java
+++ app/src/main/java/com/maxwellyeo/dankbank/Transaction.java
@@ -78,53 +78,87 @@ public class Transaction extends Activity {
         return log;
     }
 
+    /**
+     * Turns a float into a undo log status message.
+     * @param balance input float
+     * @return String in log status message.
+     */
+    protected String toUndoLog(Float balance) {
+        String dollar = toDollar(balance);
+        String log = "Decreased transaction amount by " + dollar;
+        return log;
+    }
+
     /* 1 Cent Button Event */
     public void addCent(View view) {
         trans += 0.01f;
         msg.setText(toDollar(trans));
         log_status.setText(toLog(0.01f));
+        stack.push(0.01f);
     }
     /* 5 Cent Button Event */
     public void addNickel(View view) {
         trans += 0.05f;
         msg.setText(toDollar(trans));
         log_status.setText(toLog(0.05f));
+        stack.push(0.05f);
     }
     /* 10 Cent Button Event */
     public void addDime(View view) {
         trans += 0.10f;
         msg.setText(toDollar(trans));
         log_status.setText(toLog(0.10f));
+        stack.push(0.10f);
     }
     /* 20 Cent Button Event */
     public void addQuarter(View view) {
         trans += 0.25f;
         msg.setText(toDollar(trans));
         log_status.setText(toLog(0.25f));
+        stack.push(0.25f);
     }
     /* 1 Dollar Button Event */
     public void add1dollar(View view) {
         trans += 1.0f;
         msg.setText(toDollar(trans));
         log_status.setText(toLog(1.0f));
+        stack.push(1.0f);
     }
     /* 5 Dollar Button Event */
     public void add5dollar(View view) {
         trans += 5.0f;
         msg.setText(toDollar(trans));
         log_status.setText(toLog(5.0f));
+        stack.push(5.0f);
     }
     /* 10 Dollar Button Event */
     public void add10dollar(View view) {
         trans += 10.0f;
         msg.setText(toDollar(trans));
         log_status.setText(toLog(10.0f));
+        stack.push(10.0f);
     }
     /* 20 Dollar Button Event */
     public void add20dollar(View view) {
         trans += 20.0f;
         msg.setText(toDollar(trans));
         log_status.setText(toLog(20.0f));
+        stack.push(20.0f);
+    }
+
+    /**
+     * Undoes last addition
+     * @param view
+     */
+    public void undo(View view) {
+        if (!stack.empty()) {
+            Float undo = stack.pop();
+            trans -= undo;
+            msg.setText(toDollar(trans));
+            log_status.setText(toUndoLog(undo));
+        } else {
+            log_status.setText(R.string.first_move);
+        }
     }
 }
 
